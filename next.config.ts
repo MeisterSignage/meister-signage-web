68325b96d8d8502a9a741bd3d6c242f0861b189b
Chris Meister <meister@MacBook-Pro-von-Chris.local>
2026-05-14 16:17:40 +0200

    Remove basePath from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "/meister-test",
   images: {
     unoptimized: true,
   },
